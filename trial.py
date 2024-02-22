f315c8d36fe4b27580da45e412b20be93fa246c8
pabloalarconm <pabloalarconmoreno@gmail.com>
2024-02-22 16:30:38 +0100

    New exemplar data used for testbed

--- trial.py
+++ trial.py
@@ -3,13 +3,13 @@ import yaml
 
 from toolkit.main import Toolkit
 
-with open("toolkit/exemplar_data/CAREconfig.yaml") as file:
+with open("toolkit/exemplar_data/CAREconfig_testbed.yaml") as file:
     configuration = yaml.load(file, Loader=yaml.FullLoader)
     
 test= Toolkit()
 
-test_done = test.yaml_quality_control(input_data="toolkit/exemplar_data/yaml_preCARE.csv",configuration=configuration)
-test_done.to_csv ("toolkit/exemplar_data/yaml_CARE.csv", index = False, header=True)
+test_done = test.yaml_quality_control(input_data="toolkit/exemplar_data/yaml_preCARE_testbed.csv",configuration=configuration)
+test_done.to_csv ("toolkit/exemplar_data/yaml_CARE_testbed.csv", index = False, header=True)
 
 
 test_faib= Toolkit()
