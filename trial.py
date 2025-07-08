5aacb0bb3109114956bbb3ef5c21cc3039a29350
pabloalarconm <pabloalarconmoreno@gmail.com>
2025-07-08 11:49:27 +0200

    Updated trial.py with new tags

--- trial.py
+++ trial.py
@@ -1,4 +1,4 @@
 from toolkit.main import Toolkit
 
 toolkit = Toolkit()
-toolkit.whole_method(folder_path="toolkit/data/", template_type="OBO") ## OMOP or OBO
+toolkit.whole_method(folder_path="toolkit/data/", template_type="OBO") ## SNOMED or OBO
